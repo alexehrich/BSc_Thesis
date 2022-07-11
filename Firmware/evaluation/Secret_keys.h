ae3d6b2de662d88e770e62476a0c6c974d510af9
Alexander Ehrich <95409438+alexehrich@users.noreply.github.com>
2022-07-11 10:45:11 +0200

    Update Secret_keys.h

--- Firmware/evaluation/Secret_keys.h
+++ Firmware/evaluation/Secret_keys.h
@@ -1,13 +1,9 @@
 //WIFI Credentials
 //-------------------------------------------
-//#define WIFI_SSID "sercommBB1422"
-//#define WIFI_PASSWORD "9CGFCDKDUE4DKSGY"
-//#define WIFI_SSID "HerthaBSC"
-//#define WIFI_PASSWORD "Berlin532"
-#define WIFI_SSID "iPhone de Alexander"
-#define WIFI_PASSWORD "108995321"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 //Firebase Real-time Database Credentials
 //------------------------------------------------------------------------------------------------
-#define FIREBASE_HOST "https://aehrich-database-default-rtdb.europe-west1.firebasedatabase.app"
-#define FIREBASE_AUTH "teTsidVGggR2RfTq5sq8aj4cu8UZqM9opXK5pi34"
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
